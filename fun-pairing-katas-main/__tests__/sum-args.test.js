59671caeecfcb7d955a80fdabf43ceaec9b6f971
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2023-04-01 19:08:19 +0100

    sumArgs() MVP tested and complete

---
+++ fun-pairing-katas-main/__tests__/sum-args.test.js
@@ -0,0 +1,16 @@
+const sumArgs = require('../katas/sum-args');
+
+describe('sumArgs()', () => {
+  test('should return 0 if given no args', () => {
+    expect(sumArgs()).toEqual(0);
+  });
+  test('should return a single arg', () => {
+    expect(sumArgs(3)).toEqual(3);
+  });
+  test('should return to sum of two arguments', () => {
+    expect(sumArgs(5, 7)).toEqual(12);
+  });
+  test('should return to sum of many arguments', () => {
+    expect(sumArgs(1, 2, 3, 4, 5)).toEqual(15);
+  });
+});
